39c2ee39bb72fe864a7413c5028287c3736573ac
Kaami Dev <kaami@kaamidev.com>
2020-11-06 17:27:44 -0500

    coded all code add commands

--- app.js
+++ app.js
@@ -43,11 +43,97 @@ client.on('message', (message) => {
 					.setThumbnail('https://i.imgur.com/h1SU6XO.png')
 					.addField(
 						'Bot Commands',
-						'`a!set <code>` - Set your affiliate code to be used.\n`a!create <link>` - Create an affiliate link for a product.'
+						'`a!set <code>` - Set your affiliate code to be used.\n`a!remove` - Remove your affiliate code.\n`a!code` - View your currently set affiliate code.\n`a!create <link>` - Create an affiliate link for a product.'
 					)
 					.setTimestamp()
 					.setFooter('Amazon Affiliates', 'https://i.imgur.com/h1SU6XO.png')
 			);
+		} else if (message.content.split(' ')[0] === 'set') {
+			// extract code from command
+			let code = message.content.split(' ')[1];
+
+			// make sure code exists
+			if (code) {
+				// check if user already has code set
+				if (db.codes.findOne({ user: message.author.id })) {
+					// prompt to first remove code
+					message.channel.send(
+						createErr(
+							'You already have an affiliate code set.\nTo add a new one, please first remove it using `a!remove`',
+							message.member.user.tag,
+							message.member.user.avatarURL()
+						)
+					);
+				} else {
+					// save code to db
+					db.codes.save({
+						code,
+						user: message.author.id
+					});
+
+					// display success
+					message.channel.send(
+						createMessage(
+							'Your affiliate code is now set to `' + code + '`',
+							message.member.user.tag,
+							message.member.user.avatarURL()
+						)
+					);
+				}
+			} else {
+				// display invalid
+				message.channel.send(
+					createErr('Invalid code provided.', message.member.user.tag, message.member.user.avatarURL())
+				);
+			}
+		} else if (message.content === 'remove') {
+			// check if user has affiliate code set
+			if (db.codes.findOne({ user: message.author.id })) {
+				// remove code from db
+				db.codes.remove({ user: message.author.id });
+
+				// display success
+				message.channel.send(
+					createMessage(
+						'Your affiliate code was removed successfully!',
+						message.member.user.tag,
+						message.member.user.avatarURL()
+					)
+				);
+			} else {
+				// prompt to first set code
+				message.channel.send(
+					createErr(
+						'No affiliate code currently set.\nSet one using `a!set <code>`',
+						message.member.user.tag,
+						message.member.user.avatarURL()
+					)
+				);
+			}
+		} else if (message.content === 'code') {
+			// retrieve code from db
+			let code = db.codes.findOne({ user: message.author.id });
+
+			// check if user has code set
+			if (code) {
+				// display code
+				message.channel.send(
+					createMessage(
+						'Your affiliate code is currently set to `' + code.code + '`',
+						message.member.user.tag,
+						message.member.user.avatarURL()
+					)
+				);
+			} else {
+				// prompt to first set code
+				message.channel.send(
+					createErr(
+						'No affiliate code currently set.\nSet one using `a!set <code>`',
+						message.member.user.tag,
+						message.member.user.avatarURL()
+					)
+				);
+			}
 		}
 	}
 });
